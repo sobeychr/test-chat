b43d0bc3c5bd8f8ce502849147a6ff9d62a388d1
Christopher Sobey <christopher.sobey@mindgeek.com>
2018-11-09 16:49:38 -0500

    + Added global Header

---
+++ src/component/element/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './../../style/element/header.scss';
+
+const Header = () => (
+    <header>
+        <h1>HEAD</h1>
+    </header>
+);
+
+export default Header;
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Header from './component/element/header';
+
 import Index from './component/page/index';
 import Test from './component/page/test';
 import Page404 from './component/page/page404';
 
 const Routes = () => (
     <Router>
-        <Switch>
-            <Route exact path="/" component={Index} />
-            <Route exact path="/test" component={Test} />
-            <Route component={Page404} />
-        </Switch>
+        <div>
+            <Header/>
+            
+            <Switch>
+                <Route exact path="/" component={Index} />
+                <Route path="/test" component={Test} />
+                <Route component={Page404} />
+            </Switch>
+        </div>
     </Router>
 );
 
