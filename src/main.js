47862af9f3591ae27094b0c56d5e1b6454006a35
Christopher Sobey <christopher.sobey@mindgeek.com>
2018-11-09 14:19:42 -0500

    - Adjusting basic setup ! Able to run Localhost:8081

---
+++ src/main.js
@@ -0,0 +1,13 @@
+/*
+import React form 'react';
+import ReactDOM from 'react-dom';
+
+// import Index from './components/Index';
+
+const app = document.getElementById('app');
+ReactDOM.render(<h1>test<h1>, app);
+*/
+
+(function(){
+    console.log('testing');
+}());
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,37 @@
 const path = require('path');
+const Htmlwb = require('html-webpack-plugin');
 
+module.exports = {
+    entry: path.join(__dirname,'src','main.js'),
+    output: {
+        path: path.join(__dirname,'web'),
+        filename: 'src.min.js'
+    },
+
+    mode: process.env.NODE_ENV || 'development',
+
+    resolve: {
+        modules: [
+            path.resolve(__dirname, 'src'),
+            'node_modules'
+        ]
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname,'web')
+    },
+
+    plugins: [
+        /*
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname,'src','index.html')
+        })
+        */
+    ]
+};
+
+
+/*
 module.exports = {
     mode:  'development',
     entry: './src/main.js',
@@ -16,7 +48,7 @@ module.exports = {
                 ],
                 loader: "babel-loader",
                 options: {
-                    presets: ["es2015"]
+                    presets: ['react-app']
                 }
             }
         ]
@@ -27,3 +59,4 @@ module.exports = {
         path: path.resolve(__dirname, 'web')
     }
 };
+*/
