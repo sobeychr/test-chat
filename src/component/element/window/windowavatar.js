b36dc047bca85e1d11b463de79ca01e515dde95b
Christopher Sobey <christopher.sobey@mindgeek.com>
2018-11-14 09:29:17 -0500

    - Quick edit on Package.json - Changed "grid" into "avatarData" in WindowAvatar + Added excludes in AvatarData

--- src/component/element/window/windowavatar.js
+++ src/component/element/window/windowavatar.js
@@ -3,31 +3,31 @@ import PropTypes from 'prop-types';
 
 import './../../../style/element/window/windowavatar.scss';
 
-const grid = require('./../../../data/avatar.json');
+const avatarData = require('./../../../data/avatar.json');
 
 class WindowAvatar extends React.Component {
     getColumn() {
-        const imgCount = this.getImg() * this.getMaxGrid(),
-              rowCount = this.getRow() * grid.rows,
+        const imgCount = this.getImg() * this.getMaxavatarData(),
+              rowCount = this.getRow() * avatarData.rows,
               counter = this.props.id - imgCount - rowCount;
         return counter;
     }
     getImg() {
-        return Math.floor(this.props.id / this.getMaxGrid());
+        return Math.floor(this.props.id / this.getMaxavatarData());
     }
-    getMaxGrid() {
-        return grid.columns * grid.rows;
+    getMaxavatarData() {
+        return avatarData.columns * avatarData.rows;
     }
     getRow() {
-        const imgCount = this.getImg() * this.getMaxGrid(),
+        const imgCount = this.getImg() * this.getMaxavatarData(),
               counter = this.props.id - imgCount;
-        return Math.floor(counter / grid.rows);
+        return Math.floor(counter / avatarData.rows);
     }
 
     render() {
         const className = 'windowavatar' + this.getImg(),
-              posX = -1 * grid.newWidth  * this.getColumn(),
-              posY = -1 * grid.newHeight * this.getRow(),
+              posX = -1 * avatarData.newWidth  * this.getColumn(),
+              posY = -1 * avatarData.newHeight * this.getRow(),
               style = {
                 backgroundPosition: posX + 'px ' + posY + 'px'
               };
