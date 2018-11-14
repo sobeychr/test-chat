38c95080360992dd8ab9ab441048f527b26206f2
Christopher Sobey <christopher.sobey@mindgeek.com>
2018-11-14 14:25:45 -0500

    + Added basic WindowChat

--- src/component/element/window/windowchat.js
+++ src/component/element/window/windowchat.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+import './../../../style/element/window/windowchat.scss';
+
+const WindowChat = ({name, avatar, status, window}) => {
+    const styles = {
+        left: window.x,
+        top: window.y,
+        width: window.width,
+        height: window.height
+    };
+
+    return (
+        <div className="windowchat" style={styles}>
+            <p className="name">{name}</p>
+            <pre>chat window</pre>
+        </div>
+    );
+};
+
+/*
 class WindowChat extends React.Component {
     render() {
         return (
@@ -9,3 +29,23 @@ class WindowChat extends React.Component {
         );
     }
 }
+*/
+
+export default WindowChat;
+
+WindowChat.defaultProps = {
+    name: "",
+    avatar: 0,
+    status: 0,
+    "window": {
+        x: 50,
+        y: 50,
+        width: 200,
+        height: 200
+    }
+};
+WindowChat.propTypes = {
+    name: PropTypes.string.isRequired,
+    avatar: PropTypes.number,
+    status: PropTypes.number
+};
--- src/component/page/index.js
+++ src/component/page/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-import WindowAvatar from './../element/window/windowavatar.js';
+import WindowChat from './../element/window/windowchat.js';
 
 import './../../style/page/index.scss';
 
+const userData = require('./../../data/user.json');
+
 class Index extends React.Component {
     render() {
+        /*
         const avatars = Array.from(
                 {length: 6*6*6},
                 (_, i) => <WindowAvatar key={i} id={i} />
@@ -14,6 +17,16 @@ class Index extends React.Component {
         return (
             <div className="main">
                 {avatars}
+            </div>
+        );
+        */
+        const users = userData.map(
+                (data, i) => <WindowChat key={i} {...data}/>
+            );
+
+        return (
+            <div className="main">
+                {users}
             </div>
         );
     }
