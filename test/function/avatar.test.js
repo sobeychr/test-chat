44ef29d3aa674ac6af6a16d0a17664d38f418eeb
Christopher Sobey <christopher.sobey@mindgeek.com>
2018-12-13 12:24:21 -0500

    + Completed basic setup for tests - Re-adjusted WindowContent > from tests + Populated tests

--- test/function/avatar.test.js
+++ test/function/avatar.test.js
@@ -1,6 +1,66 @@
 import * as fc from 'Function/windowcontent';
 
-test('getExtension - gif', () => {
+const extImage   = ['jpg','jpeg','png','gif','svg','webp'];
+const extVideo   = ['gifv','mp4','webm'];
+const extInvalid = ['doc', 'exe', 'gitignore'];
 
-    expect( fc.getExtension('test.gif') ).toBe('gif');
+const query = '?param=2175&test=w9ouevb&isbdv_=iaouvb-_92efg';
+const hash  = '#lisufdbv20-+=0f';
+
+const exts  = []
+    .concat(extImage)
+    .concat(extVideo)
+    .concat(extInvalid);
+const paths = [
+    'test.',
+    'http://test.',
+    'https://alpha/test.',
+    '//domain.test-vm/test.'
+];
+const queries = [
+    '',
+    query,
+    hash,
+    query + hash,
+    hash + query
+];
+
+describe('WindowContent tests', () => {
+    exts.forEach(tExt => {
+        paths.forEach(tPath => {
+            queries.forEach(tQuery => {
+
+                const url = tPath + tExt + tQuery;
+                const isInvalid = extInvalid.indexOf(tExt) >= 0;
+                const isLink = tPath.indexOf('//') >= 0;
+
+                test('getExtension - ' + tExt + ', "' + url + '"', async() => {
+                    if(isInvalid) {
+                        expect( fc.getExtension(url) ).toBeNull();
+                    }
+                    else {
+                        expect( fc.getExtension(url) ).toBe(tExt);
+                    }
+                });
+
+                test('isMedia - ' + tExt + ', "' + url + '"', async() => {
+                    if(isInvalid) {
+                        expect( fc.isMedia(url) ).toBeFalsy();
+                    }
+                    else {
+                        expect( fc.isMedia(url) ).toBeTruthy();   
+                    }
+                });
+
+                test('isHyperlink - ' + tExt + ', "' + url + '"', async() => {
+                    if(isLink) {
+                        expect( fc.isHyperlink(url) ).toBeTruthy();
+                    }
+                    else {
+                        expect( fc.isHyperlink(url) ).toBeFalsy();   
+                    }
+                });
+            });
+        });
+    });
 });
