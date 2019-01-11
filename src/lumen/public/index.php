8bfdec3508fa4dce8dda48898dd8fbdd81b398b2
Christopher Sobey <christopher.sobey@mindgeek.com>
2019-01-11 16:33:28 -0500

    + Added GitIgnore on Cache folder

--- src/lumen/public/index.php
+++ src/lumen/public/index.php
@@ -3,6 +3,7 @@
 if(!defined('PATH_ROOT'))   define('PATH_ROOT',   realpath(__DIR__ . './../') . DIRECTORY_SEPARATOR);
 
 if(!defined('PATH_DATA'))   define('PATH_DATA',   realpath(PATH_ROOT . './../data/') . DIRECTORY_SEPARATOR);
+if(!defined('PATH_CACHE'))  define('PATH_CACHE',  PATH_DATA . 'cache' . DIRECTORY_SEPARATOR);
 if(!defined('PATH_IMAGE'))  define('PATH_IMAGE',  realpath(PATH_ROOT . './../image/') . DIRECTORY_SEPARATOR);
 if(!defined('PATH_PUBLIC')) define('PATH_PUBLIC', PATH_ROOT . 'public' . DIRECTORY_SEPARATOR);
 
